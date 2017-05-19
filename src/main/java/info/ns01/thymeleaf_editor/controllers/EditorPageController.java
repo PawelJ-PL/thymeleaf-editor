5bcee600c1ca172304339fdd0ef5edf404c2b277
Pawel <inne.poczta@gmail.com>
2017-05-19 15:02:34 +0200

    flash messages list

--- src/main/java/info/ns01/thymeleaf_editor/controllers/EditorPageController.java
+++ src/main/java/info/ns01/thymeleaf_editor/controllers/EditorPageController.java
@@ -6,12 +6,12 @@ import info.ns01.thymeleaf_editor.models.enums.FlashMessageType;
 import info.ns01.thymeleaf_editor.services.TemplateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.thymeleaf.exceptions.TemplateInputException;
 
 import javax.validation.Valid;
 
@@ -28,20 +28,31 @@ public class EditorPageController {
     private TemplateService templateService;
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String editorForm(TemplateForm templateForm, Model model) {
+    public String editorForm(TemplateForm templateForm) {
         return "editor/form";
     }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
     public String handleForm(@Valid TemplateForm templateForm, BindingResult bindingResult,
                              RedirectAttributes redirectAttributes) {
+        List<FlashMessage> messages = new ArrayList<>();
         if (bindingResult.hasErrors()) {
-            List<FlashMessage> messages = new ArrayList<>();
             for (ObjectError error : bindingResult.getAllErrors()) {
                 messages.add(new FlashMessage(FlashMessageType.WARNING, error.getDefaultMessage()));
             }
             redirectAttributes.addFlashAttribute(STANDARD_FLASH_ATTRIBUTE_NAME, messages);
+            return "redirect:/editor";
         }
+
+        try {
+            String result = templateService.processTemplate(templateForm.getTemplate(), "HTML5");
+        } catch (TemplateInputException err) {
+            messages.add(new FlashMessage(FlashMessageType.DANGER, err.getMessage()));
+            redirectAttributes.addFlashAttribute(STANDARD_FLASH_ATTRIBUTE_NAME, messages);
+            return "redirect:/editor";
+        }
+
+        redirectAttributes.addFlashAttribute(templateForm);
         return "redirect:/editor";
     }
     
